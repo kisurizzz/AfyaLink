2c181bc4541dd8a28f2db8a12abd8a1be9080cc7
Arnold Kisuri <arnoldkisuri7@gmail.com>
2025-04-29 13:44:21 +0300

    Add deployed back end api link

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:5000/api";
+const API_URL = "https://afyalinkbackend.onrender.com";
 
 // Helper function to get headers with token
 const getHeaders = (token) => ({
